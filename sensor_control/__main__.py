386f77e4fe31ef648fd457ec1555d2473793a690
Adrian Schneider <github@ardy.io>
2018-06-13 06:31:31 +1000

    untangle wrapper class from function to make implementing additional interfaces easier

--- sensor_control/__main__.py
+++ sensor_control/__main__.py
@@ -1,7 +1,7 @@
 import os
 import fire
 from importlib import import_module
-from sensor_control.prop_methods import property_getters_setters
+from sensor_control.prop_methods import wrap_properties
 
 def main(noreset=False):
     from sensor_control.ar0330.ar0330 import Ar0330 as Sensor
@@ -11,7 +11,7 @@ def main(noreset=False):
             pass
         Sensor._reset = nop
 
-    s = property_getters_setters(Sensor)
+    s = wrap_properties(Sensor)
     fire.Fire(s)
 
 if __name__ == "__main__":
