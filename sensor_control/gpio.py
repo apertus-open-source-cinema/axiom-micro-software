8224bb3d1562e02db2831c0fae3fd6f2457eb2f6
Adrian Schneider <github@ardy.io>
2018-07-10 20:35:56 +1000

    add class for gpio access

---
+++ sensor_control/gpio.py
@@ -0,0 +1,12 @@
+from subprocess import check_output
+
+class GPIO:
+    def __init__(self, address):
+        self.addr = address
+        
+    def set(value=0xdeadbeef):
+        cmd = "devmem2 0x%x w" % self.addr
+        if value != 0xdeadbeef:
+            cmd += " 0x%x" % value
+
+        check_output(cmd, shell=True)
