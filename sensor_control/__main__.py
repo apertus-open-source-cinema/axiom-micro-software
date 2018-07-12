da7fd3f4b59c8019c8886984e5b8c3ef27c2415e
Adrian Schneider <github@ardy.io>
2018-07-12 22:38:43 +1000

    add pfusch so sensor_control can be run without an actual sensor
    add environment variable SENSORCTL_NORESET to skip sensor reset sequence
    when starting up

--- sensor_control/__main__.py
+++ sensor_control/__main__.py
@@ -1,9 +1,22 @@
-
+import os
+import fire
 from importlib import import_module
+from sensor_control.prop_methods import property_getters_setters
 
-def main():
+def main(noreset=False):
     from sensor_control.ar0330.ar0330 import Ar0330 as Sensor
-    s = Sensor()
+
+    if noreset:
+        def nop(*args, **kwargs):
+            pass
+        Sensor._reset = nop
+
+    s = property_getters_setters(Sensor)
+    fire.Fire(s)
 
 if __name__ == "__main__":
-    main()
+    kwargs = {}
+    if os.environ.get("SENSORCTL_NORESET", 0):
+        kwargs["noreset"] = True
+        
+    main(**kwargs)
