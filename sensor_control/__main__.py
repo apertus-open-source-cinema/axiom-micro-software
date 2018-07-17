8a676920517b81180ee72725fc16428be2b9273a
Adrian Schneider <github@ardy.io>
2018-07-17 22:01:37 +1000

    move sensor to config file

--- sensor_control/__main__.py
+++ sensor_control/__main__.py
@@ -1,9 +1,16 @@
 import os
 import fire
+from yaml import load
 from importlib import import_module
+from sensor_control.util import RelativeOpener
 
-def get_wrapper(wrapper="CliWrapper", sensor="Ar0330"):
+relative_opener = RelativeOpener(__file__)
+conf = load(relative_opener.open("config.yml"))
+
+def get_wrapper(wrapper="CliWrapper", sensor=None):
     import sensor_control.wrappers
+    if sensor is None:
+        sensor = conf["sensor"]
     sensor_mod = import_module("sensor_control." + sensor.lower() + "." + sensor.lower())
 
     wrapper = getattr(sensor_control.wrappers, wrapper)
