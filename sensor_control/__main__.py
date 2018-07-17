ac72818c6609f6e6cf929501ada59a461f4884d9
Adrian Schneider <github@ardy.io>
2018-07-17 17:29:45 +1000

    make wrapper and sensor customizable with command line arguments

--- sensor_control/__main__.py
+++ sensor_control/__main__.py
@@ -3,6 +3,14 @@ import fire
 from importlib import import_module
 from sensor_control.wrappers import CliWrapper
 
+def get_wrapper(wrapper="CliWrapper", sensor="Ar0330"):
+    import sensor_control.wrappers
+    sensor_mod = import_module("sensor_control." + sensor.lower() + "." + sensor.lower())
+
+    wrapper = getattr(sensor_control.wrappers, wrapper)
+    sensor = getattr(sensor_mod, sensor)
+    return wrapper(sensor)
+
 def main(noreset=False):
     from sensor_control.ar0330.ar0330 import Ar0330 as Sensor
 
@@ -11,8 +19,7 @@ def main(noreset=False):
             pass
         Sensor._reset = nop
 
-    s = CliWrapper(Sensor)
-    fire.Fire(s)
+    fire.Fire(get_wrapper)
 
 if __name__ == "__main__":
     kwargs = {}
