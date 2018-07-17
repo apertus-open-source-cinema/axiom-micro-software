9725a160c9e628e7ccbe027d98c13e49ea1fbf58
Adrian Schneider <github@ardy.io>
2018-07-17 21:35:19 +1000

    cleanup: remove leftover debug code

--- sensor_control/__main__.py
+++ sensor_control/__main__.py
@@ -1,7 +1,6 @@
 import os
 import fire
 from importlib import import_module
-from sensor_control.wrappers import CliWrapper
 
 def get_wrapper(wrapper="CliWrapper", sensor="Ar0330"):
     import sensor_control.wrappers
@@ -11,19 +10,8 @@ def get_wrapper(wrapper="CliWrapper", sensor="Ar0330"):
     sensor = getattr(sensor_mod, sensor)
     return wrapper(sensor)
 
-def main(noreset=False):
-    from sensor_control.ar0330.ar0330 import Ar0330 as Sensor
-
-    if noreset:
-        def nop(*args, **kwargs):
-            pass
-        Sensor._reset = nop
-
+def main():
     fire.Fire(get_wrapper)
 
 if __name__ == "__main__":
-    kwargs = {}
-    if os.environ.get("SENSORCTL_NORESET", 0):
-        kwargs["noreset"] = True
-        
-    main(**kwargs)
+    main()
