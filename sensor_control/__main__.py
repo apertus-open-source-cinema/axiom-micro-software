1e44120b196ab02a0dba3e1a6adde171a10815fd
Adrian Schneider <github@ardy.io>
2018-08-14 21:46:46 +0200

    expose properties via fuse

--- sensor_control/__main__.py
+++ sensor_control/__main__.py
@@ -1,24 +1,33 @@
 import os
-import fire
+import sys
+import argparse
 from yaml import load
 from importlib import import_module
 from sensor_control.util import RelativeOpener
+from sensor_control.propfs import expose_properties
 
 relative_opener = RelativeOpener(__file__)
 conf = load(relative_opener.open("config.yml"))
 
-def get_wrapper(wrapper="CliWrapper", sensor=None):
-    import sensor_control.wrappers
-    if sensor is None:
-        sensor = conf["sensor"]
+def get_sensor(sensor):
     sensor_mod = import_module("sensor_control." + sensor.lower() + "." + sensor.lower())
 
-    wrapper = getattr(sensor_control.wrappers, wrapper)
     sensor = getattr(sensor_mod, sensor)
-    return wrapper(sensor)
+    return sensor
 
 def main():
-    fire.Fire(get_wrapper)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--sensor', default=conf["sensor"])
+    parser.add_argument('fuseargs', nargs=argparse.REMAINDER)
+    args = parser.parse_args()
+
+    sensor = get_sensor(args.sensor)()
+
+    # fuse-python eats sys.argv, so we just shove the rest of the args there
+    args.fuseargs.insert(0, sys.argv[0])
+    sys.argv = args.fuseargs
+    breakpoint()
+    expose_properties(sensor)
 
 if __name__ == "__main__":
     main()
